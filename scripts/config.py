aede17b5a4b4c28fa1bd408365a3a540c24995ee
Joseph Zhang <jzhang.joe03@gmail.com>
2026-06-16 19:46:43 -0400

    chore(config): add API configuration parameters

--- scripts/config.py
+++ scripts/config.py
@@ -2,6 +2,11 @@ from utils.path import get_git_root
 
 BASE_DIR = get_git_root()
 
+# config data directories
 DATA_DIR = BASE_DIR / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 TMP_DATA_DIR = DATA_DIR / "temp"
+
+# config API parameters
+API_PUB_BASE_URL = "https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action"
+PACKAGE_PARAMS = {"id": "bike-share-toronto"}
