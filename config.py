dec658be59c9e50976d7a553bc0bedf457426fef
Joseph Zhang <jzhang.joe03@gmail.com>
2026-07-16 04:53:59 -0400

    chore: adjust config import path to root dir

---
+++ config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+# config data directories
+DATA_DIR = BASE_DIR / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+TMP_DATA_DIR = DATA_DIR / "temp"
+
+# config API parameters
+CKAN_FEED = "https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action"
+GBFS_FEED = "https://toronto.publicbikesystem.net/customer/gbfs/v3.0"
+CKAN_RT_PACKAGE_PARAMS = {"id": "bike-share-toronto"}
+HIST_PACKAGE_PARAMS = {"id": "bike-share-toronto-ridership-data"}
